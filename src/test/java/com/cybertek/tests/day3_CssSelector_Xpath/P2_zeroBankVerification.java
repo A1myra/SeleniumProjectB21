42d0efa61515f2b48549dfffdc7cc51d9c12f6f1
Almira H <amiraa1225@gmail.com>
2021-01-12 12:47:33 -0500

    Initial commit

---
+++ src/test/java/com/cybertek/tests/day3_CssSelector_Xpath/P2_zeroBankVerification.java
@@ -0,0 +1,4 @@
+package com.cybertek.tests.day3_CssSelector_Xpath;
+
+public class P2_zeroBankVerification {
+}
---
+++ src/test/java/com/cybertek/tests/day4_findElements_checkboxes/P1_Xpath_CssSelector.java
@@ -0,0 +1,42 @@
+package com.cybertek.tests.day4_findElements_checkboxes;
+
+public class P1_Xpath_CssSelector {
+
+        public static void main(String[] args) {
+            //TC #1: PracticeCybertek.com_ForgotPassword WebElement verification
+            //1. Open Chrome browser
+            /*
+            WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+            //2. Go to http://practice.cybertekschool.com/forgot_password
+            driver.get("http://practice.cybertekschool.com/forgot_password");
+
+            //3. Locate all the WebElements on the page using XPATH and/or CSS locator only (total of 6)
+
+            //a. “Home” link
+            WebElement homeLink = driver.findElement(By.xpath("//a[.='Home']"));
+
+            //Locating the same link using class attribute's value
+            //WebElement homeLink = driver.findElement(By.xpath("//a[@class='nav-link']"));
+
+            //Locating the same link using href attribute's value
+            //WebElement homeLink = driver.findElement(By.xpath("//a[@href='/']"));
+
+            //b. “Forgot password” header
+            WebElement forgotPasswordHeader = driver.findElement(By.xpath("//h2[.='Forgot Password']"));
+
+            //c. “E-mail” text
+            WebElement emailLabel = driver.findElement(By.xpath("//label[@for='email']"));
+
+            //Locating the label using its TEXT with XPATH
+            //WebElement emailLabel = driver.findElement(By.xpath("//label[.='E-mail']"));
+
+            //d. E-mail input box
+            //e. “Retrieve password” button
+            //f. “Powered by Cybertek School” text
+            //4. Verify all WebElements are displayed.
+
+             */
+        }
+    }
+
